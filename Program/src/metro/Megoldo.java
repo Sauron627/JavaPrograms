fd32a76ba46d94f2ad082fd6e7b6fabb898769fc
Kovács Gábor <gabor.kovacs@optin.hu>
2014-11-17 15:39:30 +0100

    2014 nov 17. adatok

--- Program/src/metro/Megoldo.java
+++ Program/src/metro/Megoldo.java
@@ -2,37 +2,61 @@ package metro;
 
 public class Megoldo {
 	
-		public void megalloszam(){
-					
-			String[]  allomas= new String[12];
-			String[]	allomasok_ossz = new String[12];
-			int k =0;
-			for(String[] megallok : MetroReader.allomasok){
-				for(String megallo: megallok){
+		public static String[]  allomas= new String[(MetroReader.metro+MetroReader.villamos)*4];
+		
+		public void allomasFeltolto(){
+			
+			int k = 0;
+			for(String[] megallok : MetroReader.allomasok)
+			{
+				for(String megallo: megallok)
+				{
 					allomas[k] = megallo;
 					k++;
 				}
 			}
+		}
+	
+		public void megalloszam(){
+			
+			String[]	allomasok_ossz = new String[(MetroReader.metro+MetroReader.villamos)*4];
+			int l=0;
+			int ossz =0;
+			boolean letezik = false;
+			
+			
+			
 			for(int i=0;i<12;i++)
 			{
-				for(k =0;k<12;k++){
-					if(allomas[i] != allomasok_ossz[k]) allomasok_ossz[i] = allomas[i];
+				letezik = false;
+				
+				for(int j=0; j<12;j++)
+				{
+						if( allomas[i].equals(allomasok_ossz[j]))
+						{
+							letezik=true;
+						}
+				}
+				
+				if(letezik == false)
+				{
+					allomasok_ossz[l]=allomas[i];
+					l++;
+					ossz+=1;
 				}
 			}
 			
-			for(String s : allomasok_ossz){
-				System.out.println(s);
-			}
+			System.out.printf("%d\n",ossz);
 			
 			
-			
-		
 		}
 		
 		public void leghosszabbVonalak(){
+			
 			int metroLeghosszabb = 0;
 			int villamosLeghosszabb = 0;
 			int k = 0;
+			
 			for(int i=0;i<MetroReader.metro;i++)
 			{
 				for(String s: MetroReader.allomasok[i])
@@ -42,7 +66,9 @@ public class Megoldo {
 				if(metroLeghosszabb <= k) metroLeghosszabb= k;
 				k=0;
 			}
+			
 			k=0;
+			
 			for(int j = MetroReader.metro; j<(MetroReader.metro+MetroReader.villamos);j++)
 			{
 				for(String s: MetroReader.allomasok[j])
@@ -50,15 +76,51 @@ public class Megoldo {
 					k+=1;	
 				}
 				if(villamosLeghosszabb <= k) villamosLeghosszabb= k;
+				
 				k=0;
 			}
 			System.out.printf("%d %d\n", metroLeghosszabb,villamosLeghosszabb);
 		}
-		public void csakMetroval(){
+		
+		
+		public void csakMetroval()
+		{
+			
+			String[] MetroMegallo = new String[4];
+			boolean letezik = false;
+			int i;
+			
+			for(int m =0; m<4 ; m++){
+				 MetroMegallo[m]= MetroReader.allomasok[MetroReader.metro][m];
+			}
+			
+			for(i=0;i<4;i++)
+			{
+				letezik = false;
+				
+				for(int j=0; j<8;j++)
+				{
+						if( MetroMegallo[i].equals(allomas[j]))
+						{
+							letezik=true;
+						}
+				}
+				
+				if(letezik == false)
+				{
+					
+					System.out.printf("%s ", MetroMegallo[i]);
+				}
+			}	
+		}
+		
+		public void leghosszabbUt()
+		{
 			
 		}
+		
 		public Megoldo(){
-			;
+			allomasFeltolto();
 			
 		}
 }
